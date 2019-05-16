f6aa11e1411c9ba2614ae2f1b575dc822c9e024c
Manvi Gupta <35099417+Manvi07@users.noreply.github.com>
2019-05-16 23:50:32 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -9,7 +9,7 @@ D = []
 header = next(csv_f)
 for row in csv_f:
     if math.isnan(float(row[8])) == False:
-        D.append( (row[0], 10**((float(row[8]) + 5)//5) ) )
+        D.append( (row[0], 10**((float(row[8]) + 5)/5) ) )
 
 #D.sort(reverse = True)
 Starlist = sorted(D, key=lambda x: x[1])
