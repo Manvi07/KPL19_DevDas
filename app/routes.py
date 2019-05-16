853938026c88d524ce41ccebac8142898888e62d
Swapnil Rustagi <swapnilrustagi97792@gmail.com>
2019-05-16 23:47:05 +0530

    Integrated problem 1

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from problem1 import Problem1
+
+import csv
+import math
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/prob1')
+@app.route('/prob1', methods=['GET', 'POST'])
 def prob1():
+    if request.method=='POST':
+        starID = request.form['starID']
+        starlist = Problem1(starID)
+        return render_template('prob1result.html', results=starlist, starID=starID)
     return render_template('prob1.html')
+    
