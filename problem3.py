235f405057b7b53c222695573a329f6accb69f32
Milind712000 <email2milindtopno.test@gmail.com>
2019-05-16 22:54:49 +0530

    form tuple of id and D

--- problem3.py
+++ problem3.py
@@ -9,6 +9,11 @@ D = []
 header = next(csv_f)
 for row in csv_f:
     if math.isnan(float(row[8])) == False:
-        D.append(10**((float(row[8]) + 5)//5))
+        D.append( (row[0], 10**((float(row[8]) + 5)//5) ) )
 
-D.sort(reverse = True)
+#D.sort(reverse = True)
+Starlist = sorted(D, key=lambda x: x[1])
+
+print('five dimmest: ')
+for i in Starlist[:5]:
+    print(i[0], i[1])
